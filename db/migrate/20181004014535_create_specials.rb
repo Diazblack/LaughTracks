d6fb317f9968916b37526105aee3ff888795d5b3
Cesar Jolibois <diazcd85@gmail.com>
2018-10-04 13:33:39 -0600

    updated special model, migrations, and seeds files

--- db/migrate/20181004014535_create_specials.rb
+++ db/migrate/20181004014535_create_specials.rb
@@ -2,8 +2,10 @@ class CreateSpecials < ActiveRecord::Migration[5.2]
   def change
     create_table :specials do |t|
       t.text :name
-      t.integer :length #time in minutes
+      t.integer :run_time_length #time in minutes
       t.text :image_url
+      t.integer :comedian_id
+      t.integer :year
 
       t.timestamps null: false
     end

a66638b8936b704f9787b619aa8708a9ce97c387
Cesar Jolibois <diazcd85@gmail.com>
2018-10-04 13:44:51 -0600

    succesfully created database

--- db/migrate/20181004014535_create_specials.rb
+++ db/migrate/20181004014535_create_specials.rb
@@ -4,9 +4,9 @@ class CreateSpecials < ActiveRecord::Migration[5.2]
       t.text :name
       t.integer :run_time_length #time in minutes
       t.text :image_url
-      t.integer :comedian_id
       t.integer :year
-
+      t.integer :comedian_id
+      
       t.timestamps null: false
     end
   end
